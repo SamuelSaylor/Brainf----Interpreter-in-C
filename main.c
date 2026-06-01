d54b97ed4af2f8fbec1351820e38344a745bcdb3
SamuelSaylor <samuelsaylor2025@gmail.com>
2026-06-01 14:53:10 -0500

    setup prior to code

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//argc is argument count. argv is the argument vector
+int main(int argc, char *argv[]) {
+    
+    switch(/*todo*/){
+
+        case '>': break; //moves cursor right
+        case '<': break; //moves cursor left
+        case '+': break; //increments cell value
+        case '-': break; //decrements cell value
+        case '.': break; //outputs cell value
+        case ',': break; //inputs value into cell
+        case '[': break; //start loop
+        case ']': break; //end loop
+
+    }
+
+
+
+    return 0;
+}
