846bee429bcdedf949f2b807b0a9f3d8b882be0c
SamuelSaylor <samuelsaylor2025@gmail.com>
2026-06-01 15:30:10 -0500

    finished switch logic for brackets. should all be functional now.

--- main.c
+++ main.c
@@ -5,7 +5,6 @@
 
 char tape[TAPE_SIZE];
 int tape_pointer = 0;
-
 int *bracket_map;
 
 //argc is argument count. argv is the argument vector
@@ -24,19 +23,30 @@ int main(int argc, char *argv[]) {
     fclose(f);
     
     bracket_map = malloc(size * sizeof(int));
-
+    int *stack = malloc(size * sizeof(int));
+    int stack_pointer = 0;
+    for(int i = 0; i < size; i++){
+        if(program[i] == '['){
+            stack[stack_pointer++] = i; //in python terms im basically appending here
+        }else if (program[i] == ']'){
+            int new_point = stack[--stack_pointer] = i; //and ther eright here im popping
+            bracket_map[new_point] = i;
+            bracket_map[i] = new_point;
+        }
+    }
+    free(stack);
 
     int cnt = 0;
     while(program[cnt]){
         switch(program[cnt]){
-            case '>': tape_pointer++;                 break; //moves cursor right
-            case '<': tape_pointer--;                 break; //moves cursor left
-            case '+': tape[tape_pointer]++;           break; //increments cell value
-            case '-': tape[tape_pointer]--;           break; //decrements cell value
-            case '.': putchar(tape[tape_pointer]);    break; //outputs cell value
-            case ',': tape[tape_pointer] = getchar(); break; //inputs value into cell
-            case '[': break; //start loop
-            case ']': break; //end loop
+            case '>': tape_pointer++;                                 break; //moves cursor right
+            case '<': tape_pointer--;                                 break; //moves cursor left
+            case '+': tape[tape_pointer]++;                           break; //increments cell value
+            case '-': tape[tape_pointer]--;                           break; //decrements cell value
+            case '.': putchar(tape[tape_pointer]);                    break; //outputs cell value
+            case ',': tape[tape_pointer] = getchar();                 break; //inputs value into cell
+            case '[': if(!tape[tape_pointer]) cnt = bracket_map[cnt]; break; //start loop
+            case ']': if(tape[tape_pointer]) cnt = bracket_map[cnt];  break; //end loop
 
             //yes i felt fancy and evened out the break for aesthetic affects
         }  
@@ -44,4 +54,5 @@ int main(int argc, char *argv[]) {
     }
 
     return 0;
+    
 }
